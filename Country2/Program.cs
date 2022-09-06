b4ed7b79ec24ad1aa0364b3988c1892f516b9345
NarminSH <nshivakhanova@gmail.com>
2022-09-06 07:35:34 +0400

    change type

--- Country2/Program.cs
+++ Country2/Program.cs
@@ -5,17 +5,13 @@ using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-using HttpClient phone = new()
-{
-    BaseAddress = new Uri("http://country.io/phone.json"),
-};
+using HttpClient client = new();
 
-await GetData(phone);
-
-
+await GetData(client);
 
 static async Task GetData(HttpClient client)
 {
+    List<Country> countries = new List<Country>();
     using HttpResponseMessage phoneResponse = await client.GetAsync("http://country.io/phone.json");
     using HttpResponseMessage nameResponse = await client.GetAsync("http://country.io/names.json");
     using HttpResponseMessage isoResponse = await client.GetAsync("http://country.io/iso3.json");
@@ -30,73 +26,49 @@ static async Task GetData(HttpClient client)
     currencyResponse.EnsureSuccessStatusCode();
     capitalResponse.EnsureSuccessStatusCode();
 
-    var jsonResponse1 = await phoneResponse.Content.ReadAsStringAsync();
+    var jsonResponse1 = phoneResponse.Content.ReadAsStringAsync().Result;
+    var jsonResponse2 = nameResponse.Content.ReadAsStringAsync().Result;
+    var jsonResponse3 = isoResponse.Content.ReadAsStringAsync().Result;
+    var jsonResponse4 = continentResponse.Content.ReadAsStringAsync().Result;
+    var jsonResponse5 = currencyResponse.Content.ReadAsStringAsync().Result;
+    var jsonResponse6 = capitalResponse.Content.ReadAsStringAsync().Result;
 
-    List<string> nameList = jsonResponse1.Split(',').ToList();
+    var phone = JObject.Parse(jsonResponse1);
+    var name = JsonConvert.DeserializeObject(jsonResponse2);
+    var iso = JsonConvert.DeserializeObject(jsonResponse3);
+    var continent = JsonConvert.DeserializeObject(jsonResponse4);
+    var currency = JsonConvert.DeserializeObject(jsonResponse5);
+    var capital = JsonConvert.DeserializeObject(jsonResponse6);
 
-    var jsonResponse2 = await nameResponse.Content.ReadAsStringAsync();
-    List<string> nameList2 = jsonResponse2.Split(',').ToList();
-    
 
-    var jsonResponse3 = await isoResponse.Content.ReadAsStringAsync();
-    List<string> nameList3 = jsonResponse3.Split(',').ToList();
-
-    var jsonResponse4 = await continentResponse.Content.ReadAsStringAsync();
-    List<string> nameList4 = jsonResponse4.Split(',').ToList();
-
-    var jsonResponse5 = await currencyResponse.Content.ReadAsStringAsync();
-    List<string> nameList5 = jsonResponse5.Split(',').ToList();
-
-    var jsonResponse6 = await capitalResponse.Content.ReadAsStringAsync();
-    List<string> nameList6 = jsonResponse6.Split(',').ToList();
-    var new1 = nameList.Zip(nameList2);
-    var new2 = new1.Zip(nameList3);
-    var new3 = new2.Zip(nameList4);
-    var new4 = new3.Zip(nameList5);
-
-    foreach (var item in new4)
+    for (int i = 0; i < phone.Count; i++)
     {
-        Console.WriteLine(item);
+        var obj = new Country
+        {
+            Phone = new List<DataPair>(),
+            Name = new List<DataPair>(),
+            Continent = new List<DataPair>(),
+            Currency = new List<DataPair>(),
+            ISO = new List<DataPair>()
+        };
+
+        foreach (var item in phone)
+        {
+            obj.Phone.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
+            obj.ISO.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
+            obj.Name.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
+            obj.Currency.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
+            obj.Continent.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
+        }
+        countries.Add(obj);
+
+    }
+    foreach (var item in countries)
+    {
+        Console.WriteLine("{0,-10}{1,-10}{2,-15}{3,-15}{4,-25}{5,-15}{6,-15}", item.Phone, item.ISO, item.Capital, item.Currency, item.Continent, item.Name);
     }
 
-
-    //for (int i = 0; i < new4.Count(); i++)
-    //{
-    //    var obj = new Country
-    //    {
-    //        Phone = new List<DataPair>(),
-    //        Name = new List<DataPair>(),
-    //        Continent = new List<DataPair>(),
-    //        Currency = new List<DataPair>(),
-    //        ISO = new List<DataPair>()
-    //    };
-
-    //    foreach (var item in new4)
-    //    {
-    //        obj.Phone.Add(new DataPair() { Value = item.First });
-    //        obj.ISO.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
-    //        obj.Name.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
-    //        obj.Currency.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
-    //        obj.Continent.Add(new DataPair() { Key = item.Key, Value = item.Value.ToString() });
-    //    }
-    //    objects.Add(obj);
-    //    foreach (var item in objects)
-    //    {
-    //        foreach (var pair in item.Phone)
-    //        {
-    //            Console.WriteLine($"Name={pair.Value}");
-    //        }
-    //        foreach (var pair in item.Name)
-    //        {
-    //            Console.WriteLine($"Name={pair.Value}");
-    //        }
-
-    //    }
-    //}
-
 }
-    
-
 
 /*
 var rawObj = JObject.Parse(phoneResponse);
